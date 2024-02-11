da9fa92c43423feed29606553fe89554ccbcdbe6
Vlaimir Korba <krissky1986@gmail.com>
2024-02-11 15:55:53 +0200

    09-Styling View Template

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  // styles: [
+  //   'a{text-decoration: none; margin: 0 10px;}',
+  //   'button{padding: 10px 20px;}',
+  //   '.ekart--header{width: 100%; height:70px;}',
+  //   '#id{}'
+  // ],
 })
 export class HeaderComponent {}
