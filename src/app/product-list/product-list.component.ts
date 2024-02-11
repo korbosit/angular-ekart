1524ac9bfcec9614d8323024cfa0257b8f87b603
Vlaimir Korba <krissky1986@gmail.com>
2024-02-11 23:34:20 +0200

    17- Event Binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,13 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  name = 'John Doe';
+  // name = 'John Doe';
+  addToCart: number = 0;
   product = {
     name: 'Iphone X',
     price: 789,
     color: 'Black',
     discount: 8.5,
-    inStock: 5,
+    inStock: 10,
     pImage: '/assets/images/iphone.png',
   };
 
@@ -22,5 +23,19 @@ export class ProductListComponent {
     return (
       this.product.price - (this.product.price * this.product.discount) / 100
     );
+  }
+
+  onNameChange(event: any) {
+    // this.name = event.target.value;
+    // console.log(event.target.value);
+  }
+
+  decrementCartValue() {
+    if (this.addToCart > 0) {
+      this.addToCart--;
+    }
+  }
+  incrementCartValue() {
+    if (this.addToCart < this.product.inStock) this.addToCart++;
   }
 }
