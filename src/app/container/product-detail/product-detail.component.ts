1d67b4719dba8e25c304d94a936cd97fad55c2b8
Vlaimir Korba <krissky1986@gmail.com>
2024-02-18 15:21:30 +0200

    #53.2 Conditional Attribute Directive | Creating & Using Custom Directive |

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -4,11 +4,18 @@ import { Product } from '../../Models/Product';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { SetBackground } from '../../CustomDirectives/SetBackground.directive';
 import { AppHoverDirective } from '../../CustomDirectives/app-hover.directive';
+import { DisableProductDirective } from '../../CustomDirectives/disable-product.directive';
 
 @Component({
   selector: 'product-detail',
   standalone: true,
-  imports: [CommonModule, NgTemplateOutlet, SetBackground, AppHoverDirective],
+  imports: [
+    CommonModule,
+    NgTemplateOutlet,
+    SetBackground,
+    AppHoverDirective,
+    DisableProductDirective,
+  ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
